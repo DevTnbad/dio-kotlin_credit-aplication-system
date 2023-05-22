9fef2fc4d0e71bf7839126ad2cb466ce84eba5b0
DevTnbad <dev.tnbad@gmail.com>
2023-05-22 10:56:24 -0300

    finalizando_curso

--- src/main/kotlin/me/dio/credit/request/system/dto/CreditDto.kt
+++ src/main/kotlin/me/dio/credit/request/system/dto/CreditDto.kt
@@ -1,15 +1,23 @@
 package me.dio.credit.request.system.dto
 
+import jakarta.validation.constraints.Future
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import me.dio.credit.request.system.entity.Credit
 import me.dio.credit.request.system.entity.Customer
 import java.math.BigDecimal
 import java.time.LocalDate
 
 data class CreditDto(
-        val creditValue: BigDecimal,
+        @field:NotNull val creditValue: BigDecimal,
+
+        @field:Future(message = "A data tem que ser futura")
         val dayFirstOfInstallment: LocalDate,
-        val numberOfInstallments: Int,
-        val customerId: Long
+
+        @field:Positive @field:Max(value  = 48) val numberOfInstallments: Int,
+
+        @field:NotNull val customerId: Long
 ) {
         fun toEntity(): Credit = Credit(
                 creditValue = this.creditValue,
--- src/main/kotlin/me/dio/credit/request/system/dto/CustomerUpdateDto.kt
+++ src/main/kotlin/me/dio/credit/request/system/dto/CustomerUpdateDto.kt
@@ -1,14 +1,16 @@
 package me.dio.credit.request.system.dto
 
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
 import me.dio.credit.request.system.entity.Customer
 import java.math.BigDecimal
 
 data class CustomerUpdateDto(
-        val firstName: String,
-        val lastName: String,
-        val income: BigDecimal,
-        val zipcode: String,
-        val street: String
+        @field:NotEmpty(message = "O campo deve ser preenchido.") val firstName: String,
+        @field:NotEmpty(message = "O campo deve ser preenchido.") val lastName: String,
+        @field:NotNull val income: BigDecimal,
+        @field:NotEmpty(message = "O campo deve ser preenchido.") val zipcode: String,
+        @field:NotEmpty(message = "O campo deve ser preenchido.") val street: String
 ) {
     fun toEntity(customer: Customer): Customer {
         customer.firstName  = this.firstName
